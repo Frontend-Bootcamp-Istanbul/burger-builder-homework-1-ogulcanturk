f879f0a30bcf6bc1bd5b4a12153d3e184a6f4791
Oğulcan TÜRK <oglcnturk@gmail.com>
2019-11-09 16:29:33 +0300

    add list

--- src/components/TotalPrice/index.js
+++ src/components/TotalPrice/index.js
@@ -1,5 +1,3 @@
-/**
- * Created by mehmetyurtar on 11/9/19.
- */
 import TotalPrice from "./TotalPrice";
+
 export default TotalPrice;
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,5 @@
-/**
- * Created by mehmetyurtar on 11/9/19.
- */
 import Hamburger from "./Hamburger";
 import TotalPrice from "./TotalPrice";
+import List from "./List";
 
-export {Hamburger, TotalPrice};
+export {Hamburger, TotalPrice, List};
